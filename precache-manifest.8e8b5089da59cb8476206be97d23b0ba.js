2b03ddfc8c9f8283b9e073d1745e18b4b61c3546
onezer00 <onezer00@users.noreply.github.com>
2022-08-06 03:33:58 +0000

    deploy: b9b10921fa808382f8358ce28231a0f5e87125a3

---
+++ precache-manifest.8e8b5089da59cb8476206be97d23b0ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b44d7260774acbb360a9",
+    "url": "github-page/static/css/main.c8bd5dbd.chunk.css"
+  },
+  {
+    "revision": "b44d7260774acbb360a9",
+    "url": "github-page/static/js/main.b44d7260.chunk.js"
+  },
+  {
+    "revision": "e387ce0610b005cb1fe3",
+    "url": "github-page/static/js/runtime~main.e387ce06.js"
+  },
+  {
+    "revision": "5035361b13f412b4eee5",
+    "url": "github-page/static/js/2.5035361b.chunk.js"
+  },
+  {
+    "revision": "88ca98e650d852663176392bc1443a05",
+    "url": "github-page/static/media/oner.88ca98e6.png"
+  },
+  {
+    "revision": "1396c4af0df7047fa4b6bc3d96418f3b",
+    "url": "github-page/index.html"
+  }
+];
